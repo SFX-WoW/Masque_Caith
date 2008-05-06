ae296cb70175bdf6044e2a06079ecec97954abf1
StormFX <stormfx@gmail.com>
2008-05-06 20:03:09 +0000

    - Update files.

---
+++ ReadMe.txt
@@ -0,0 +1,14 @@
+IMPORTANT:
+- You MUST have the most recent revision of ButtonFacade installed in order to use this skin pack.
+
+About:
+- This is a skin pack for ButtonFacade that will add some custom button styles to the supported action bars.
+
+Installation:
+- Download the package. For the most recent version, please check http://files.wowace.com.
+- Unzip the package to your WoW/Interface/AddOns/ directory.
+
+Feedback:
+- Please direct all questions and comments to the WoWAce.com forums.
+
+This package is not to be included in any compilation or redistributed without the permission of the author(s).
---
+++ Skins.lua
@@ -0,0 +1,99 @@
+--[[ Caith for ButtonFacade v2.4.2 ]]
+
+local LibButtonFacade = LibStub("LibButtonFacade",true)
+if not LibButtonFacade then
+	return
+end
+
+-- Caith
+LibButtonFacade:AddSkin("Caith",{
+
+	-- Skin data start.
+	Backdrop = {
+		Width = 40,
+		Height = 40,
+		Texture = [[Interface\AddOns\ButtonFacade_Caith\Textures\Backdrop]],
+	},
+	Icon = {
+		Width = 32,
+		Height = 32,
+		TexCoords = {0.07,0.93,0.07,0.93},
+	},
+	Border = {
+		Width = 40,
+		Height = 40,
+		Texture = [[Interface\AddOns\ButtonFacade_Caith\Textures\Equip]],
+		BlendMode = "ADD",
+	},
+	Flash = {
+		Width = 40,
+		Height = 40,
+		Texture = [[Interface\AddOns\ButtonFacade_Caith\Textures\Flash]],
+	},
+	Cooldown = {
+		Width = 32,
+		Height = 32,
+	},
+	AutoCast = {
+		Width = 32,
+		Height = 32,
+		ModelScale = 1.0,
+	},
+	AutoCastable = {
+		Width = 64,
+		Height = 64,
+		Texture = [[Interface\Buttons\UI-AutoCastableOverlay]],
+		OffsetX = 0.5,
+		OffsetY = -0.5,
+	},
+	Normal = {
+		Width = 40,
+		Height = 40,
+		Texture = [[Interface\AddOns\ButtonFacade_Caith\Textures\Normal]],
+		Static = true,
+	},
+	Pushed = {
+		Width = 40,
+		Height = 40,
+		Texture = [[Interface\AddOns\ButtonFacade_Caith\Textures\Pushed]],
+	},
+	Disabled = {
+		Hide = true,
+	},
+	Checked = {
+		Width = 40,
+		Height = 40,
+		Texture = [[Interface\AddOns\ButtonFacade_Caith\Textures\Checked]],
+		BlendMode = "ADD",
+	},
+	Gloss = {
+		Width = 40,
+		Height = 40,
+		Texture = [[Interface\AddOns\ButtonFacade_Caith\Textures\Gloss]],
+	},
+	Highlight = {
+		Width = 40,
+		Height = 40,
+		Texture = [[Interface\AddOns\ButtonFacade_Caith\Textures\Highlight]],
+		BlendMode = "ADD",
+	},
+	HotKey = {
+		Width = 40,
+		Height = 10,
+		OffsetX = -4,
+		OffsetY = 8,
+	},
+	Count = {
+		Width = 40,
+		Height = 10,
+		OffsetX = -4,
+		OffsetY = -8,
+	},
+	Name = {
+		Width = 40,
+		Height = 10,
+		OffsetY = -8,
+	},
+	-- Skin data end.
+
+},true)

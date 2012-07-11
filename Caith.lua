5c6da0035874f3264eabd06071414e5e7cbe6177
StormFX <stormfx@gmail.com>
2012-07-11 15:03:34 +0000

    - ToC to 40300. - Update version.

--- Caith.lua
+++ Caith.lua
@@ -15,9 +15,9 @@ if not MSQ then return end
 -- Caith
 MSQ:AddSkin("Caith", {
 	Author = "StormFX",
-	Version = "4.2.@project-revision@",
+	Version = "4.3.@project-revision@",
 	Shape = "Square",
-	Masque_Version = 40200,
+	Masque_Version = 40300,
 	Backdrop = {
 		Width = 42,
 		Height = 42,
